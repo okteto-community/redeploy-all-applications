2633bc2eebea1465a26473c891e6d54ee1169941
Ramiro Berrelleza <ramiro@okteto.com>
2025-01-16 14:59:03 -0800

    add option to skip sleeping namespaces

--- app/model/namespace.go
+++ app/model/namespace.go
@@ -1,7 +1,19 @@
 package model
 
+type Status string
+
+const (
+	Active           = "Active"
+	DestroyAllFailed = "DestroyAllFailed"
+	DestroyingAll    = "DestroyingAll"
+	Deleting         = "Deleting"
+	Inactive         = "Inactive"
+	Sleeping         = "Sleeping"
+	DeleteFailed     = "DeleteFailed"
+)
+
 // Namespace represents an Okteto namespace
 type Namespace struct {
 	Name   string `json:"name"`
-	Status string `json:"status"`
+	Status Status `json:"status"`
 }
